bc50acff3209ce4eec8b79316b0df70a32042d11
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-03-11 15:05:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# VS Code TOSA
+
+**TOSA** is **O**pen **S**ource **A**vailable.
+
+A [Visual Studio Code](https://code.visualstudio.com/) [extension](https://marketplace.visualstudio.com/VSCode) which open pull request page from line you selected. You can more easily find why the code is included by the page:mag_right:
+
+<img src="https://user-images.githubusercontent.com/3317191/37252237-0752e564-2561-11e8-8028-662393dbb05c.png" width="600px" />
+
+If you want to run not only on VS Code also CLI? You can get CLI version from [here](https://github.com/kyoshidajp/tosa). 
+
+## Extension Settings
+
+This extension needs to specify the following settings:
+
+* `vscodetosa.token`: GitHub API TOKEN
+
+<img width="500" alt="vscodetosa_settings" src="https://user-images.githubusercontent.com/3317191/37252324-b567b00c-2562-11e8-89af-74ad23ff6864.png">
+
+## Author
+
+[Katsuhiko YOSHIDA](https://github.com/kyoshidajp)
---
+++ src/gitclient.ts
@@ -0,0 +1,96 @@
+import { window, workspace, commands, StatusBarAlignment, Uri } from 'vscode';
+import child_process = require('child_process');
+import path = require('path');
+
+import { GIT_PATH, CONFIG_NAME } from './constants';
+
+export class GitClient {
+
+    private _statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+    private octokit = require('@octokit/rest')();
+
+    constructor() {
+        this.initializeOctokit();
+    }
+
+    public openPR() {
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        const doc = editor.document;
+        const fileName = path.basename(doc.fileName);
+        const currentLine = editor.selection.active.line + 1;
+        const blameCommand = GIT_PATH;
+        const args = `blame -L ${currentLine},${currentLine} ${fileName}`.split(" ");
+        const currentDirectory = path.dirname(doc.fileName);
+        const gitExecOptions = {
+            cwd: currentDirectory
+        };
+        child_process.execFile(blameCommand, args, gitExecOptions, (error, stdout, stderror) => {
+            if (error !== null) {
+                window.showErrorMessage(stderror.toString());
+                return;
+            }
+
+            this.openPage(stdout);
+        });
+    }
+
+    public dispose() {
+    }
+
+    private initializeOctokit():void {
+        const config = workspace.getConfiguration(CONFIG_NAME);
+        const token = config['token'];
+        if (!token) {
+            this.showError(`Could not found ${CONFIG_NAME}.token in settings.`);
+            return;
+        }
+
+        this.octokit.authenticate({
+            type: 'integration',
+            token: token
+        });
+    }
+
+    private showError(message: string) {
+        window.showErrorMessage(message);
+        console.info(message);
+    }
+
+    private async openPage(out: string) {
+        let sha = out.split(" ")[0];
+        if (sha === "") {
+            console.error(`Invalid sha. Sha: ${sha}`);
+            return;
+        }
+
+        this.octokit.search.issues({q: sha}, (error: any, result: any) => {
+            if (error) {
+                this.showSerachPRError(error);
+                return;
+            }
+
+            let items = result.data.items;
+            if (items.length === 0) {
+                this.showError(`Could not find Pull Request. Sha: ${sha}`);
+                return;
+            }
+    
+            let pr = items[items.length - 1];
+            let url = pr.pull_request.html_url;
+            commands.executeCommand('vscode.open', Uri.parse(url));
+        });
+    }
+
+    private showSerachPRError(error: any) {
+        let message = "Unknown error was occured while searching the Pull Request.";
+        if (error.message) {
+            message += `\nErrorMessage: ${error.message}`;
+        }
+        this.showError(message);
+    }
+}
