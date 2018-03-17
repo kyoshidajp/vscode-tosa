05fd9f8bf9135c6e1ce6e92294b80e040de392c7
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-03-17 13:13:46 +0900

    Add constructor test

--- src/test/githubclient.test.ts
+++ src/test/githubclient.test.ts
@@ -1,9 +1,21 @@
 import * as assert from 'assert';
 import nock = require('nock');
+import sinon = require('sinon');
 
 import { GithubClient } from '../githubclient';
 
 suite("GithubClient Tests", () => {
+
+    let tokenIndex = 0;
+    const stub = sinon.stub(GithubClient.prototype, <any>'getToken');
+
+    test("constructor", () => {
+        stub.onCall(tokenIndex++).returns("1231");
+        assert.ok(new GithubClient());
+
+        stub.onCall(tokenIndex++).returns("");
+        assert.throws(() => {return new GithubClient();}, Error);
+    });
 
     test("getFullRepositoryName", () => {
         assert.equal("kyoshidajp/vscode-tosa",
@@ -35,7 +47,9 @@ suite("GithubClient Tests", () => {
                 ]
             });
 
+        stub.onCall(tokenIndex++).returns("1231");
         const client = new GithubClient();
+        
         assert.equal("https://github.com/kyoshidajp/vscode-tosa/pull/1",
             await client.getPullRequestUrl("bc50acff3209ce4eec8b79316b0df70a32042d11",
                 "kyoshidajp/vscode-tosa"
